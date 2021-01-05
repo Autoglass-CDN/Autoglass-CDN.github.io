ce336b840b8454d62f67bd2ccd1fb4122a04a1a9
patrick-braga <patrick.braga@autoglass.com.br>
2021-01-05 16:07:37 -0300

    B2C-JC - Corrige erro ao renderizar o componente de cep

--- hml/cep.component.js
+++ hml/cep.component.js
@@ -49,7 +49,7 @@ $(function CepComponent() {
 				View.renderCepInfo(
 					cepContainer,
 					modalContent,
-					orderForm.shippingData.address
+					orderForm.shippingData?.address
 				);
 
 				$(window).on(CONFIG.EVENTS.CEP_UPDATED, async (e) => {
@@ -147,35 +147,35 @@ $(function CepComponent() {
 				$(cepContainer)
 					.removeClass('ghost-loading')
 					.html(`
-            <div class="cep-info">
-              <div class="cep-info__location">
-                <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-geo-alt" fill="currentColor"
-                  xmlns="http://www.w3.org/2000/svg">
-                  <path fill-rule="evenodd"
-                    d="M12.166 8.94C12.696 7.867 13 6.862 13 6A5 5 0 0 0 3 6c0 .862.305 1.867.834 2.94.524 1.062 1.234 2.12 1.96 3.07A31.481 31.481 0 0 0 8 14.58l.208-.22a31.493 31.493 0 0 0 1.998-2.35c.726-.95 1.436-2.008 1.96-3.07zM8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10z" />
-                  <path fill-rule="evenodd" d="M8 8a2 2 0 1 0 0-4 2 2 0 0 0 0 4zm0 1a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
-                </svg>
-                <span class="cep-info__location-street">${addressFormatted}</span>
-              </div>
-              <button id="change-cep-button" class="cep-info__location-button">Alterar</button>
-            </div>
-          `);
+						<div class="cep-info">
+						<div class="cep-info__location">
+							<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-geo-alt" fill="currentColor"
+							xmlns="http://www.w3.org/2000/svg">
+							<path fill-rule="evenodd"
+								d="M12.166 8.94C12.696 7.867 13 6.862 13 6A5 5 0 0 0 3 6c0 .862.305 1.867.834 2.94.524 1.062 1.234 2.12 1.96 3.07A31.481 31.481 0 0 0 8 14.58l.208-.22a31.493 31.493 0 0 0 1.998-2.35c.726-.95 1.436-2.008 1.96-3.07zM8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10z" />
+							<path fill-rule="evenodd" d="M8 8a2 2 0 1 0 0-4 2 2 0 0 0 0 4zm0 1a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
+							</svg>
+							<span class="cep-info__location-street">${addressFormatted}</span>
+						</div>
+						<button id="change-cep-button" class="cep-info__location-button">Alterar</button>
+						</div>
+					`);
 			} else {
 				$(cepContainer)
 					.removeClass('ghost-loading')
 					.html(`
-            <div class="cep-info">
-              <div class="cep-info__location">
-                <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-geo-alt" fill="currentColor"
-                  xmlns="http://www.w3.org/2000/svg">
-                  <path fill-rule="evenodd"
-                    d="M12.166 8.94C12.696 7.867 13 6.862 13 6A5 5 0 0 0 3 6c0 .862.305 1.867.834 2.94.524 1.062 1.234 2.12 1.96 3.07A31.481 31.481 0 0 0 8 14.58l.208-.22a31.493 31.493 0 0 0 1.998-2.35c.726-.95 1.436-2.008 1.96-3.07zM8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10z" />
-                  <path fill-rule="evenodd" d="M8 8a2 2 0 1 0 0-4 2 2 0 0 0 0 4zm0 1a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
-                </svg>
-              </div>
-              <button id="change-cep-button" class="cep-info__location-button">Informe seu cep aqui</button>
-            </div>
-          `);
+						<div class="cep-info">
+						<div class="cep-info__location">
+							<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-geo-alt" fill="currentColor"
+							xmlns="http://www.w3.org/2000/svg">
+							<path fill-rule="evenodd"
+								d="M12.166 8.94C12.696 7.867 13 6.862 13 6A5 5 0 0 0 3 6c0 .862.305 1.867.834 2.94.524 1.062 1.234 2.12 1.96 3.07A31.481 31.481 0 0 0 8 14.58l.208-.22a31.493 31.493 0 0 0 1.998-2.35c.726-.95 1.436-2.008 1.96-3.07zM8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10z" />
+							<path fill-rule="evenodd" d="M8 8a2 2 0 1 0 0-4 2 2 0 0 0 0 4zm0 1a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
+							</svg>
+						</div>
+						<button id="change-cep-button" class="cep-info__location-button">Informe seu cep aqui</button>
+						</div>
+					`);
 			}
 
 			$(`#${cepContainer.id} .cep-info__location-button`).click(() => {
