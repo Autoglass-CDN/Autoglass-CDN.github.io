0dfe0b09e5b7da4229813dfb32e0d68908b1484b
Giovane Merisio <giovane.merisio@autoglass.com.br>
2021-02-03 16:58:28 -0300

    B2C-1837 - Testando inclusão do video da categoria 5

--- dev/js/galeria-imagem-video.dev.js
+++ dev/js/galeria-imagem-video.dev.js
@@ -194,11 +194,13 @@ function ImageControl1(a, pi) {
         
         /* Adicionando class para renderizar o vídeo corretamente */
         p.attr('class', 'responsive-video');
-        
+        p.children("").html()
         // Injetando vídeo na página para ser viauzalidado na galeria de imagens
         $("[id=show] [id=include] [id=image][productIndex=" + pi + "]").append(p);
         
-        $("gtm-video-parabrisa-player").attr('style','display: flex; width: 100%');
+        $("#gtm-video-parabrisa").children("iframe").attr('style','display: flex; width: 100%');
+        
+        //$("gtm-video-parabrisa-player").attr('style','display: flex; width: 100%');
 
         // Bucando os elementos do iframe e loading
         //const iframeEle = document.getElementById('gtm-video-parabrisa-player');
