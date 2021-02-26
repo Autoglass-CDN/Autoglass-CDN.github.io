c566237b96fffaed05ad82f3390c2652e292edcb
Giovane Merisio <giovane.merisio@autoglass.com.br>
2021-02-26 15:55:10 -0300

    B2C-1837 - Corrige função na tratativa de eventos na galeria de imagens 3

--- dev/js/galeria-imagem-video.dev.js
+++ dev/js/galeria-imagem-video.dev.js
@@ -1,7 +1,7 @@
 /* Teste para galeria de imagens com vídeo */
 
 $(document).ready(function () {
-    if (Vtex != undefined){
+    if (typeof Vtex !== 'undefined'){
         ImageControl1($("ul.thumbs a:first"), 0);
     
         imageVideoGalery();
