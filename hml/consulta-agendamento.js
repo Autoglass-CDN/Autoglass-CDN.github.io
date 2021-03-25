761ad1e691239b367f47aa4a66dd4f4059b3ac24
Conrado <conradocostaad@yahoo.com.br>
2021-03-25 15:44:25 -0300

    B2C-1885_v2 - Corrigindo grafia do atributo 'delectedDeliveryChannel' em chamadas de API da VTEX para atualização do orderForm

--- hml/consulta-agendamento.js
+++ hml/consulta-agendamento.js
@@ -424,7 +424,7 @@ $(function () {
         return {
           addressId: newSelectedAddresses[0].addressId,
           itemIndex: x.itemIndex,
-          selectedDeleveryChannel: "pickup-in-point",
+          selectedDeliveryChannel: "pickup-in-point",
           selectedSla: slas[0].id,
         };
       });
