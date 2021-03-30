53533d3a71a7f9eba39d94e265d4722da31317f9
Conrado <conradocostaad@yahoo.com.br>
2021-03-30 16:09:40 -0300

    B2C-1885_v2 - Excluindo cookie VTEXSC com dompinio iniciando com '.autoglassonline...'

--- src/js/policies/header.js
+++ src/js/policies/header.js
@@ -144,6 +144,7 @@ function salvarUf(estado) {
 
 function setVtexScOnCookies(salesChannel) {
     document.cookie = 'VTEXSC'+ `=; domain=.www.autoglassonline.com.br ;Max-Age=-99999999;`;
+    document.cookie = 'VTEXSC'+ `=; domain=.autoglassonline.com.br ;Max-Age=-99999999;`;
     document.cookie = `VTEXSC=sc=${salesChannel}; expires=Sun, 1 Jan 2099 00:00:00 UTC; path=/`;
     // document.cookie = `VTEXSC=sc=${salesChannel}; expires=Sun, 1 Jan 2099 00:00:00 UTC;domain=.${location.host.replace('www.', '.www.')}; path=/`;
 
