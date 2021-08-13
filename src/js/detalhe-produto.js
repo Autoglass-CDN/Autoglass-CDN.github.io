d3023295317aaceb33255f1b8048dc99d012a5d1
Vinícius Albuquerque <vinicius.albuquerque@outlook.com>
2021-08-13 11:37:03 -0300

    B2C-2062 - Corrige uso de função assíncrona

--- src/js/detalhe-produto.js
+++ src/js/detalhe-produto.js
@@ -262,7 +262,7 @@ $(window).on("ready", async () => {
 
   //Busca de Veículos Compatíveis
   $('.veiculos-compativeis-search__search-box .veiculos-compativeis-search__search-input input')
-    .on('input', () => buscaCompativeis($(this).val()));
+    .on('input', () => await buscaCompativeis($(this).val()));
 
   async function buscaCompativeis(texto) {
     if (veiculosBuscaveis && veiculosBuscaveis.length > 0 && texto.length > 2) {
