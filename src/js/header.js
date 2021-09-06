946f8c52b78e1736e3096576e7f8b00c68fb8769
kaique-ferreira <kaique.ferreira@autoglass.com.br>
2021-09-06 11:56:22 -0300

    B2C-2068 - Insere codigo postal no cookie da vtex

--- src/js/header.js
+++ src/js/header.js
@@ -254,7 +254,22 @@ function salvarUf(estado) {
                     ...orderForm.shippingData.address,
                     logisticsInfo: orderForm.shippingData.logisticsInfo,
                 })
-            )
+            );
+            fetch('https://dev2autoglass.myvtex.com/api/sessions', {
+                    method: 'POST',
+                    headers: {
+                      'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(
+                      {
+                        "public": {
+                          "postalCode": {
+                            "value": orderForm.shippingData.postalCode
+                          }
+                        }
+                      }
+                  )
+                  });
         }
     );
     setVtexScOnCookies(estado.Sc);
