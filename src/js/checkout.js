7b1fdff8fbc1635e796c88facd9645907ca9ef0d
ronaldo-obj <oieusouronaldo@gmail.com>
2021-09-23 15:22:43 -0300

    B2C-2070 - Corrige insercao de elemento entre div

--- src/js/checkout.js
+++ src/js/checkout.js
@@ -457,7 +457,7 @@ function CrossSelling() {
 
             if(itensCrossSelling.length !== 0){
                 itensCrossSelling.forEach(e => {
-                    $("#imagensCorssSelling").append("<img src=" + e.items[0].images[0].imageUrl +">");
+                    $("#imagensCorssSelling").append("<div><img src=" + e.items[0].images[0].imageUrl +"></div>");
                 });
             }
             itensCrossSelling = [];
@@ -471,8 +471,8 @@ $('.responsive').slick({
       nextArrow: $('.next'),
     infinite: false,
     speed: 300,
-    slidesToShow: 4,
-    slidesToScroll: 4,
+    slidesToShow: 3,
+    slidesToScroll: 3,
     responsive: [
       {
         breakpoint: 1024,
