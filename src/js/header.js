16f0594180eccaed06558644157a29b5269eea7c
Mateus Machado <mbmachado@outlook.com>
2021-11-03 10:33:23 -0300

    B2C-2140 - Adiciona polyfill para String.prototype.replaceAll()

--- src/js/header.js
+++ src/js/header.js
@@ -334,3 +334,21 @@ function startLocalizationModal() {
         open: openLocalizationModal,
     };
 }
+
+/**
+ * String.prototype.replaceAll() polyfill para Safari
+ * @author Chris Ferdinandi
+ * @license MIT
+ */
+ if (!String.prototype.replaceAll) {
+	String.prototype.replaceAll = function(str, newStr) {
+
+		// If a regex pattern
+		if (Object.prototype.toString.call(str).toLowerCase() === '[object regexp]') {
+			return this.replace(str, newStr);
+		}
+
+		// If a string
+		return this.replace(new RegExp(str, 'g'), newStr);
+	};
+}
