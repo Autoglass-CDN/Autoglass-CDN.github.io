6a0157406805789d865b9df3803e9608f8b510e4
Mateus Machado <mateus.machado@autoglass.com.br>
2021-11-18 17:35:41 -0300

    B2C-100 - Ajustes no Busca por placa, inclusão no campo de produto

--- arquivos/js/busca-fipe/index.js
+++ arquivos/js/busca-fipe/index.js
@@ -1,8 +1,9 @@
 (function () {
-  /** BUSCA POR VEÍCULO */
+  /** BUSCA POR PEÇA */
   const Service = ServiceAPI();
   const View = ViewAPI();
   const Controller = ControllerAPI();
+  let activeTab = '#busca-peca';
 
   const CONFIG = {
     ASYNC: {
@@ -23,10 +24,10 @@
       SELECTED: "selected",
     },
     CANT_OPEN: false,
-    ORIGIN: "https://autoglassonline.com.br", //location.origin,
+    ORIGIN: "https://dev2autoglass.myvtex.com", // location.origin,
   };
 
-  const SELECTS = [
+  const PECA_SELECTS = [
     {
       title: "Produto",
       id: "produtos-select",
@@ -77,21 +78,30 @@
   _init();
 
   async function _init() {
-    let categoryTree = await Service.getCategoryTree();
+    const categoryTree = await Service.getCategoryTree();
+    const childrenCategories = [];
 
     categoryTree
       .filter((x) => x.hasChildren)
       .forEach((x) => {
-        SELECTS[0].values.push(...x.children);
+        childrenCategories.push(...x.children);
       });
 
-    console.log(SELECTS);
+    await _initBucaPeca(childrenCategories);
+    _initBucaPlaca(childrenCategories);
+  }
+
+  async function _initBucaPeca(values) {
+
+    PECA_SELECTS[0].values = values;
+
+    console.log(PECA_SELECTS);
 
     await Controller.checkRouterParams();
 
-    View.buildList(SELECTS[0].values, SELECTS[0].id);
+    View.buildList(PECA_SELECTS[0].values, PECA_SELECTS[0].id);
 
-    SELECTS.forEach(View._initSelect_);
+    PECA_SELECTS.forEach(View._initSelect_);
 
     // Create Button Function
     $("#btn-busca-peca").click(Service.search);
@@ -130,13 +140,16 @@
         }
       });
 
-      $(`.c-busca__tab-content #${select.id} > div:first-child`)
+      /* $(`.c-busca__tab-content #${select.id} > div:first-child`)
         .focus(() => {
+          console.log('Hi');
+
           $(`.c-busca__tab-content #${select.id} > div:first-child`).on(
             "keyup",
             (event) => {
               if (event.key === "Delete" || event.key === "Backspace") {
-                const index = SELECTS.findIndex((x) => x.id === select.id);
+                console.log('Hello');
+                const index = PECA_SELECTS.findIndex((x) => x.id === select.id);
                 View.resetResults(index);
                 $(`.c-busca__tab-content #${select.id}`).click();
               }
@@ -150,7 +163,7 @@
           $(`.c-busca__tab-content #${select.id} > div:first-child`).unbind(
             "keyup"
           );
-        });
+        }); */
 
       $(`.c-busca__tab-content #${select.id} .smart-select__main-results input`)
         .on("keydown", (e) => {
@@ -294,6 +307,7 @@
           }
         })
         .on("keyup", (e) => {
+          console.log(`Filtrando`);
           if (
             !["Tab", "ArrowDown", "ArrowUp", "Enter"].find((x) => x === e.key)
           )
@@ -330,7 +344,16 @@
 
             $(event.target).addClass(CONFIG.CSS.HIGHLIGHT);
           })
-          .click((event) => Controller.addClick(event, _id));
+          .click((event) => {
+            switch (activeTab) {
+              case '#busca-peca':
+                Controller.addClick(event, _id)
+                break;
+              case '#busca-placa':
+                handleSelection(event, _id);
+                break;
+            }
+          });
 
         $(`.c-busca__tab-content  #${_id} ul li`)
           .first()
@@ -367,9 +390,9 @@
     }
 
     function resetResults(_index) {
-      for (let i = _index; i <= SELECTS.length - 1; i++) {
-        const select = SELECTS[i];
-        const nextSelect = SELECTS[i + 1];
+      for (let i = _index; i <= PECA_SELECTS.length - 1; i++) {
+        const select = PECA_SELECTS[i];
+        const nextSelect = PECA_SELECTS[i + 1];
 
         $(`.c-busca__tab-content  #${select.id} > div > span`).html(
           select.title
@@ -395,9 +418,9 @@
     }
 
     function createNavigation(_class, new_title) {
-      const index = SELECTS.findIndex((x) => x.id === _class);
-      const select = SELECTS[index];
-      const nextSelect = SELECTS[index + 1];
+      const index = PECA_SELECTS.findIndex((x) => x.id === _class);
+      const select = PECA_SELECTS[index];
+      const nextSelect = PECA_SELECTS[index + 1];
 
       $(`.c-busca__tab-content  #${select.id} > div > span`).html(new_title);
       $(
@@ -468,9 +491,9 @@
     };
 
     async function addClick(event, _id) {
-      const index = SELECTS.findIndex((x) => x.id === _id);
-      const select = SELECTS[index];
-      const nextSelect = SELECTS[index + 1];
+      const index = PECA_SELECTS.findIndex((x) => x.id === _id);
+      const select = PECA_SELECTS[index];
+      const nextSelect = PECA_SELECTS[index + 1];
       // Não pode ser === Pq um pode ser INT e outro STRING, mas o valores são iguais;
       const optionSelected = select.values.find((x) => x.id == event.target.id);
 
@@ -499,7 +522,7 @@
           );
 
           // Caso seja Ano (Ultimo campo) tem que ser decrescente
-          index + 1 === SELECTS.length - 1
+          index + 1 === PECA_SELECTS.length - 1
             ? values.sort((a, b) => b.name.localeCompare(a.name))
             : values.sort((a, b) => a.name.localeCompare(b.name));
 
@@ -539,7 +562,7 @@
         ];
 
         for (let i = 0; i < params.length; i++) {
-          const select = SELECTS[i];
+          const select = PECA_SELECTS[i];
           const value = select.values.find((x) =>
             x.url ? x.url.includes(params[i]) : x.name.includes(params[i])
           );
@@ -582,7 +605,7 @@
     }
 
     function getPaths() {
-      return SELECTS.filter((x) => x.routeSelected)
+      return PECA_SELECTS.filter((x) => x.routeSelected)
         .map((x) =>
           x.routeSelected.includes("/")
             ? x.routeSelected
@@ -592,7 +615,7 @@
     }
 
     async function search() {
-      const index = SELECTS.filter((x) => x.routeSelected).length;
+      const index = PECA_SELECTS.filter((x) => x.routeSelected).length;
       const paths = getPaths();
       let url = CONFIG.ORIGIN;
 
@@ -653,13 +676,49 @@
 
 
   /** BUSCA POR PLACA */
+  const PLACA_SELECTS = [
+    {
+      title: "Produto",
+      id: "categoria-select",
+      values: [],
+      routeSelected: "",
+      isAsyncSearch: false,
+      asyncSearchTerm: "",
+      canBeClear: false,
+    },
+  ];
+
+  function _initBucaPlaca(values) {
+    
+    PLACA_SELECTS[0].values = values;
+
+    View.buildList(PLACA_SELECTS[0].values, PLACA_SELECTS[0].id);
+
+    View._initSelect_(PLACA_SELECTS[0]);
+
+    let btnBuscaPlaca = document.querySelector("#btn-busca-placa");
+  
+    btnBuscaPlaca.addEventListener("click", (event) => {
+      let placa = document.querySelector("#placa-input").value;
+
+      if(placa.length) {
+        buscaPorPlaca(placa);
+      } else {
+        alert('Você deve inserir pelo menos a sua placa!');
+      }
+    });
+  }
+
   let tabs = document.querySelectorAll(".c-busca__tabs li");
 
   tabs.forEach((tab) => {
-    tab.addEventListener("click", (evento) => {
-      // evento.preventDefault();
+    tab.addEventListener("click", (event) => {
+      event.preventDefault();
+
       tabs.forEach((t) => t.classList.remove("is-active"));
       tab.classList.add("is-active");
+
+      activeTab = tab.querySelector('a').attributes.href.nodeValue;
 
       let tabContentDivs = document.querySelectorAll(
         ".c-busca__tab-content"
@@ -674,14 +733,40 @@
     });
   });
 
-  let btnBuscaPlaca = document.querySelector("#btn-busca-placa");
-  
-  btnBuscaPlaca.addEventListener("click", (evento) => {
-    let placa = document.querySelector("#placa-input").value;
-    buscaPorPlaca(placa);
-  });
+  function handleSelection(event, _id) {
+    const select = PLACA_SELECTS[0];
+    const optionSelected = select.values.find((x) => x.id == event.target.id);
+
+    select.routeSelected = optionSelected.url
+          ? optionSelected.url.replace(new URL(optionSelected.url).origin, "")
+          : optionSelected.name;
+                
+    $(`.c-busca__tab-content  #${select.id} > div > span`).html(event.target.innerHTML);
+    $(
+      `.c-busca__tab-content  #${select.id} > div > .${CONFIG.CSS.ARROW_DOWN}`
+      ).hide();
+    $(`.c-busca__tab-content  #${select.id} > div > .${CONFIG.CSS.CLOSE}`)
+      .show()
+      .on("click", () => {
+        $(`.c-busca__tab-content  #${select.id} > div > span`).html(
+          select.title
+        );
+        select.routeSelected = "";
+
+        $(
+          `.c-busca__tab-content  #${select.id} > div > .${CONFIG.CSS.ARROW_DOWN}`
+        ).show();
+        $(
+          `.c-busca__tab-content  #${select.id} > div > .${CONFIG.CSS.CLOSE}`
+        ).hide();
+      });
+
+    $('.c-busca__tab-content .c-busca__input #placa-input').click().focus();
+  }
   
   async function buscaPorPlaca(placaString) {
+    const select = PLACA_SELECTS[0];
+
     const LABEL_DADOS_TABELA = {
       MONTADORA: "Marca",
       MODELO: "Modelo",
@@ -704,7 +789,8 @@
       modalDeCarregamento.mostarSpinner();
   
       const response = await fetch(
-        `https://www.keplaca.com/placa/${placaSemCaracteresEspeciais}`
+        'https://api.allorigins.win/get?url=' + encodeURIComponent(`https://www.keplaca.com/placa/${placaSemCaracteresEspeciais}`)
+        //`https://www.placafipe.com/placa/${placaSemCaracteresEspeciais}`
       );
       const html = await response.text();
       const DOM = new DOMParser().parseFromString(html, "text/html");
@@ -798,7 +884,7 @@
       );
   
       let url = "",
-        parametrosUrl = "?PS=24&map=";
+        parametrosUrl = "?PS=20&map=";
   
       if (
         !anosEncontrados.length &&
@@ -811,6 +897,11 @@
         );
   
         document.querySelector("a[href='#busca-peca']").click();
+      }
+
+      if(select.routeSelected.length) {
+        url += select.routeSelected;
+        parametrosUrl += `c,c,`;
       }
   
       if (anosEncontrados.length) {
@@ -830,7 +921,8 @@
   
       url += parametrosUrl; //`?PS=24&map=specificationFilter_${FILTROS_VTEX.ANO},specificationFilter_${FILTROS_VTEX.MONTADORA},specificationFilter_${FILTROS_VTEX.VEICULO}`;
   
-      location.href = url;
+      console.log(url);
+      // location.href = url;
     } catch (error) {
       console.log(error);
       modalDeCarregamento.ocultarSpinner();
