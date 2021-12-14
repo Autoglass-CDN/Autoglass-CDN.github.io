37283e44fac0e9716ac12ad5321548e0c1af9543
Mateus Machado <mateus.machado@autoglass.com.br>
2021-12-14 17:53:13 -0300

    B2C-2187 - Modifica regex modelos para considerar casos especiais

--- arquivos/js/busca-fipe/index.js
+++ arquivos/js/busca-fipe/index.js
@@ -1090,6 +1090,7 @@
           "Perdão pelo inconveniente! O serviço de busca por placa está fora do " +
           "ar no momento. Favor utilizar a busca por peça!"
         );
+        console.error(error);
       }
 
       modalDeCarregamento.ocultarSpinner();
@@ -1112,8 +1113,23 @@
         montadoraTerms.push('VW');
       }
 
-      const modeloSemMontadora = modelo.replace(
-        new RegExp(montadoraTerms.join('|'), "gi"), "").trim().split(" ")[0];
+      if(montadoraTerms[0].toUpperCase() === 'GM') {
+        montadoraTerms.push('CHEV');
+      }
+
+      const modeloSemMontadoraTerms = modelo.replace(
+        new RegExp(montadoraTerms.join('|'), "gi"), "").trim().split(" ");
+
+      const newVariants = ['NOVA', 'NOVO'];
+      let modeloSemMontadora = newVariants.some(
+          e => modeloSemMontadoraTerms.find((o) => o.toUpperCase() === e)
+        ) ? modeloSemMontadoraTerms[1]
+          : modeloSemMontadoraTerms[0];
+
+      if(modeloSemMontadoraTerms[0].toUpperCase() === 'PAJERO' && modeloSemMontadoraTerms[1]) {
+        modeloSemMontadora = modeloSemMontadoraTerms[0] + ' ' + modeloSemMontadoraTerms[1];
+      }
+
       const modeloSemCaractesEspeciais = modeloSemMontadora.replace(/[\W]+/gi, "");
 
       const patternMontadora = `(${montadoraTerms.join('|')})`;
@@ -1121,7 +1137,11 @@
         ? modeloSemMontadora
         : `(${modeloSemMontadora}|${modeloSemCaractesEspeciais})`;
 
-      const pattern = `${patternModelo}$|${patternMontadora} ${patternModelo}$`;
+      const isModeloStrada = modeloSemCaractesEspeciais.toUpperCase() === 'STRADA';
+
+      const pattern =  `${(isModeloStrada ? '^' : '')}${patternModelo}$|${patternMontadora} ${patternModelo}$`;
+
+      console.log(pattern);
 
       return new RegExp(pattern, "gi");
     }
