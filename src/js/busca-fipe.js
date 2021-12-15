46fec67863b35c6bbd43e26c475e6007f2de1c7d
Mateus Machado <mateus.machado@autoglass.com.br>
2021-12-15 16:34:25 -0300

    B2C-2184 - Remove filtro como produto universal

--- src/js/busca-fipe.js
+++ src/js/busca-fipe.js
@@ -1060,7 +1060,7 @@
 
     if(select.routeSelected.length) {
       const selectedRoute = select.routeSelected;
-      const universalProducts = ['/lampadas', '/filtros', '/higienizadores'];
+      const universalProducts = ['/lampadas', '/higienizadores-e-filtros/higienizadores'];
       
       if(universalProducts.some(o => selectedRoute.includes(o))) {
         return [
