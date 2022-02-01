fb4536ada1aa0c4926cd8bad0fcfc5ed7487da95
kaique-ferreira <kaique.ferreira@autoglass.com.br>
2022-02-01 13:10:38 -0300

    B2C-2193 - Volta com o ícone do whatsapp flutuante

--- src/js/whatsapp-icon.js
+++ src/js/whatsapp-icon.js
@@ -3,6 +3,22 @@
     let whatsappIconDesktop = document.querySelector('#whatsapp-icon-link-desktop');
     let whatsappIconMobile = document.querySelector('#whatsapp-icon-link-mobile');
     
+    //---------------------------------------------
+    let whatsappIcon = document.querySelector('#whatsapp-icon-link');
+    
+    whatsappIcon.addEventListener(touchEvent, (ev) => {
+        
+        ga('create', 'UA-133498560-1', 'autoglassonline.com', 'gaTracker');
+        ga('gaTracker.set', 'transport', 'beacon');
+        ga('gaTracker.send', 'event', 'WhatsApp', 'Clique', 'Rodapé +55 27 99826-0207');
+        
+    }, {passive: true});
+    
+    if(document.querySelector('.product-qd-v1-fixed-bar')) {
+        whatsappIcon.style.marginBottom = '55px'
+    }
+    //---------------------------------------------
+
     let touchEvent = 'ontouchstart' in window ? 'touchstart' : 'click';
     
     createGAWhatsapp(whatsappIconDesktop, 'Desktop');
