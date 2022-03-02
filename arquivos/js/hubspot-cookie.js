87ed6622827da9b5590fc30875e3f554ece48cc7
Mateus Machado <mateus.machado@autoglass.com.br>
2022-03-02 16:55:22 -0300

    B2C-2237 - Ajustes no Envio de Dados para o Master Data

--- arquivos/js/hubspot-cookie.js
+++ arquivos/js/hubspot-cookie.js
@@ -1,12 +1,63 @@
 (function () {
 
-  let hubspotutkSent = false;
+  let isDataSent = false;
+  let cartItems = [];
 
   $(window).on('rendered.vtexid', getEmailFromLoginFormAndSendDataToMasterData);
   $(window).on('checkoutRequestEnd.vtex', getEmailFromOrderFormAndSendDataToMasterData);
+  $(window).on('orderFormUpdated.vtex', checkIfShoppingCartHasNewItemsAndSendDataToMasterData);
+  $(window).on('load', sendOrderIdToMasterData);
+
+  async function sendOrderIdToMasterData() {
+    const locationHref = location.href;
+
+    if(locationHref.includes('checkout/orderPlaced')) {
+      const orderId = $("#order-id").text().trim();
+
+      const order = await fetch(`/api/checkout/pub/orders/${orderId}`, {
+        headers: new Headers({
+          "Content-Type": "application/json",
+        }),
+      }).then(resp => resp.json());
+
+      const email = order.clientProfileData.email;
+
+      email && sendDataToVtexMasterData(email, { orderId });
+    }
+  }
+
+  function checkIfShoppingCartHasNewItemsAndSendDataToMasterData(event, orderForm) {
+    if(orderForm.items) {
+      let currentCartItems = [];
+
+      for (let index = 0; index < orderForm.items.length; index++) {
+        currentCartItems.push({
+          refId: orderForm.items[index].productRefId,
+          quantity: orderForm.items[index].quantity,
+        });
+      }
+
+      if(
+        currentCartItems.length !== cartItems.length ||
+        cartItems.every((value, index) => {
+          value.refId !== currentCartItems[index].refId && value.quantity !== currentCartItems[index].quantity
+        })
+      ) {
+        cartItems = currentCartItems;
+
+        const orderForm = vtexjs.checkout.orderForm;
+
+        if(orderForm.clientProfileData && orderForm.clientProfileData.email) {
+          const email = orderForm.clientProfileData.email;
+
+          sendDataToVtexMasterData(email);
+        }
+      }
+    }
+  }
 
   function getEmailFromLoginFormAndSendDataToMasterData(event) {
-    if(hubspotutkSent)
+    if(isDataSent)
       return;
 
     const emailInput = document.querySelector('#vtexIdUI-form-classic-login #inputEmail');
@@ -15,30 +66,36 @@
     loginForm.addEventListener('submit', (e) => {
       const email = emailInput.value;
 
-      email && sendHubspotUtkToVtexMasterData(email);
+      email && sendDataToVtexMasterData(email);
     });
   }
 
   function getEmailFromOrderFormAndSendDataToMasterData(event, orderForm) {
     try {
-      if(hubspotutkSent)
+      if(isDataSent)
         return;
 
-      const vetexJsOrderForm = vtexjs.checkout.orderForm;
-      
-      let email = "";
+      const vtexJsOrderForm = vtexjs.checkout.orderForm;
 
-      if(orderForm && orderForm.clientProfileData)
+      let email = "";
+      let orderFormId = null;
+
+      if(orderForm && orderForm.clientProfileData) {
         email = orderForm.clientProfileData.email;
-      else if(vetexJsOrderForm.clientProfileData && vetexJsOrderForm.clientProfileData.email)
-        email = vetexJsOrderForm.clientProfileData.email;
+        orderFormId = orderForm.orderFormId;
+      } else if(vtexJsOrderForm.clientProfileData && vtexJsOrderForm.clientProfileData.email) {
+        email = vtexJsOrderForm.clientProfileData.email;
+        orderFormId = vtexJsOrderForm.orderFormId;
+      }
 
       if(!email) {
         getEmailFromPreEmailFormAndSendDataToMasterData();
         return;
       }
 
-      sendHubspotUtkToVtexMasterData(email);
+      sendDataToVtexMasterData(email, {
+        orderFormId: orderFormId,
+      });
     } catch (e) {
       console.warn('Falha ao obter e-mail para enviar o Hubspotutk ao MasterData!');
     }
@@ -50,18 +107,22 @@
     if(locationHref.includes('checkout/#/email')) {
       const preEmailForm = document.querySelector('#client-profile-data form.client-pre-email');
       const preEmailInput = document.querySelector('form.client-pre-email input#client-pre-email');
-  
+
       preEmailForm.addEventListener('submit', (e) => {
-        const email = preEmailInput.value;  
-        email && sendHubspotUtkToVtexMasterData(email);
+        const email = preEmailInput.value;
+        email && sendDataToVtexMasterData(email);
       });
     }
   }
 
-  async function sendHubspotUtkToVtexMasterData(email) {
+  async function sendDataToVtexMasterData(email, { orderFormId = null, orderId = null }) {
     try {
       const hubspotutk = $.cookie('hubspotutk');
-      
+
+      if(null === orderFormId) {
+        orderFormId = getOrderFormId();
+      }
+
       if(hubspotutk) {
         await fetch(`https://api.autoglass.com.br/api/master-datas/clientes/${email.trim()}`, {
           method: 'PUT',
@@ -70,14 +131,30 @@
           }),
           body: JSON.stringify({
             hubspotutk,
+            lastOrderInfo: {
+              items: cartItems,
+              orderFormId,
+              orderId,
+            }
           }),
         });
 
-        hubspotutkSent = true;
+        isDataSent = true;
       }
     } catch (e) {
-      console.warn('Falha ao enviar Hubspotutk ao MasterData!');
+      console.warn('Falha ao enviar dados ao MasterData!');
     }
   }
-  
+
+  function getOrderFormId() {
+    try {
+      const orderForm = vtexjs.checkout.orderForm;
+
+      return orderForm.orderFormId;
+    } catch (e) {
+
+      return null;
+    }
+  }
+
 })();
