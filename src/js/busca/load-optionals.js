d6ee493559a8a3b35818ff235147b2c48cb9f9e9
Lucas Tessinari <tessinarilucas@gmail.com>
2022-07-15 10:13:13 -0300

    B2C-2420 - Correção da chamada de api

--- src/js/busca/load-optionals.js
+++ src/js/busca/load-optionals.js
@@ -2,7 +2,7 @@
   async function loadOptionals() {
     const allSkuInThisPage = getAllCurrentSkus();
 
-    const baseUrlApi = "https://api.autoglass.com.br/";
+    const baseUrlApi = "https://api-hml.autoglass.com.br/";
     const baseUrlListaOpcionais = "integracao-b2c/api/web-app/produtos/opcionais-lista?codigosProdutos=";
 
     const urlToConsult = baseUrlApi + baseUrlListaOpcionais + allSkuInThisPage.join("&codigosProdutos=");
