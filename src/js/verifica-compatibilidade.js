c27af0c633bccc3f94796180adffb8265ee1f2e9
igorfbrandao <igor.brandao@autoglass.com.br>
2024-08-22 10:02:50 -0300

    B2C-2869 - Adiciona botão.

--- src/js/verifica-compatibilidade.js
+++ src/js/verifica-compatibilidade.js
@@ -1,1 +1,14 @@
 
+  $(document).ready(function(){
+    // Cria o HTML do botão como string
+    var buttonHTML = '<button class="botao-compatibilidade">Clique aqui</button>';
+
+    // Adiciona o botão dentro da <div> com a classe 'container-titulo-lateral-prateleira'
+    $('.container-titulo-lateral-prateleira').append(buttonHTML);
+
+    // Adiciona um evento de clique ao botão usando jQuery
+    $('.botao-compatibilidade').on('click', function() {
+        alert('Funcionou!');
+    });
+  });
+
