81120f2863153a3c74583754751bf128fc4979ca
Elioenai Nascimento <elioenai.nascimento@autoglass.com.br>
2025-01-16 14:59:42 -0300

    B2C-3508 - Minificacao dos arquivos JS e CSS solicitados em lista na historia

--- hml/js/cookie.bot.js
+++ hml/js/cookie.bot.js
@@ -1,97 +1,20 @@
-!(function () {
-  let e = 0;
-  function o() {
-    (aceitarCookiesCheckbox = $("#aceitar-cookies-checkbox")),
-      (aceitarCookieLink = $("#aceitar-cookie-link"));
-    let e = JSON.parse($.cookie("hasAcceptedCookies"));
-    aceitarCookiesCheckbox.prop("checked") &&
-      ((function e() {
-        let o = document.getElementById("cookiebanner");
-        o.style.cssText = "display:none !important";
-      })(),
-      (e.accepted = !0),
-      (e.acceptedAt = Date.now()),
-      $.cookie("hasAcceptedCookies", JSON.stringify(e), { path: "/" }));
-  }
-  !(function a() {
-    let c = location.pathname.includes("orderPlaced");
-    !(function e(o, a) {
-      let c = document.createElement("script");
-      (c.src = o), (c.onload = a), document.head.appendChild(c);
-    })(
-      "https://autoglass-cdn.github.io/hml/js/jquery.cookie.js",
-      function () {
-        let a = $.cookie("hasAcceptedCookies"),
-          t = a ? JSON.parse(a) : null;
-        if (c) {
-          let i;
-          fetch(
-            "https://api.autoglass.com.br/integracao-b2c/api/web-app/master-datas/cookies",
-            {
-              method: "POST",
-              headers:
-                ((i = new Headers()).append("Content-Type", "application/json"),
-                i),
-              body: JSON.stringify({
-                CodigoCompra: $("#order-id").html(),
-                DataAceite: t ? new Date(t.acceptedAt) : null,
-                MaiorIdade: t.accepted,
-              }),
-            }
-          ).then((e) => console.log(e));
-        } else
-          (t && t.accepted) ||
-            ($.cookie(
-              "hasAcceptedCookies",
-              JSON.stringify({
-                accepted: !1,
-                createdAt: Date.now(),
-                adulthood: !1,
-              }),
-              { path: "/" }
-            ),
-            (function e() {
-              $("body").append(`
-          <div id="cookiebanner">
-              <div id="c-left">
-                  <p class="c-header">Protegemos seus dados pessoais</p>
-                  <p class="c-message">
-                    O Grupo Autoglass, em respeito \xe0 privacidade dos seus dados pessoais e buscando a melhorar sua experi\xeancia de navega\xe7\xe3o no site,
-                    gostaria de obter o seu consentimento para coletar e utilizar cookies de navega\xe7\xe3o, que servir\xe3o para trazer conte\xfados personalizados
-                    e mais relevantes para voc\xea. Ao clicar em "Aceitar", voc\xea estar\xe1 concordando com a nossa <a href="/Institucional/privacidade" target="_blank">Pol\xedtica de Privacidade.</a>
-                  </p>
-              </div>
-              <div id="c-right">
-                <div class="aceitar-cookies-container">
-                  <input type="checkbox" id="aceitar-cookies-checkbox" />
-                  <label for="aceitar-cookies-checkbox" id="aceitar-cookies-label">Aceitar Cookies</label>
-                </div>
-                  <a id="aceitar-cookie-link" class="c-button c-button-disabled">Concordar</a>
-              </div>
-              <div style="clear:both"></div>
-          </div>
-      `);
-              let a = $("#aceitar-cookies-checkbox"),
-                c = $("#aceitar-cookie-link");
-              a.change(function () {
-                let e = this.checked;
-                c.toggleClass("c-button-disabled", !e),
-                  c.css("background-color", e ? "#000000" : "#999"),
-                  c.css("cursor", e ? "pointer" : "not-allowed");
-              }),
-                c.click(o);
-            })(),
-            (function o() {
-              let a = document.getElementById("cookiebanner"),
-                c = parseInt(a.offsetHeight);
-              (a.style.bottom = e - c + "px"),
-                (e += 4) < c
-                  ? setTimeout(function () {
-                      o();
-                    }, 1)
-                  : ((e = 0), (a.style.bottom = "10px"));
-            })());
-      }
-    );
-  })();
-})();
+!function(){var e,o;let a=0;function c(){aceitarCookiesCheckbox=$("#aceitar-cookies-checkbox"),aceitarCookieLink=$("#aceitar-cookie-link");let e=JSON.parse($.cookie("hasAcceptedCookies"));aceitarCookiesCheckbox.prop("checked")&&(document.getElementById("cookiebanner").style.cssText="display:none !important",e.accepted=!0,e.acceptedAt=Date.now(),$.cookie("hasAcceptedCookies",JSON.stringify(e),{path:"/"}))}let t,i;t=location.pathname.includes("orderPlaced"),e="https://autoglass-cdn.github.io/hml/js/jquery.cookie.js",o=function(){let e=$.cookie("hasAcceptedCookies"),o=e?JSON.parse(e):null;if(t){let i;fetch("https://api.autoglass.com.br/integracao-b2c/api/web-app/master-datas/cookies",{method:"POST",headers:((i=new Headers).append("Content-Type","application/json"),i),body:JSON.stringify({CodigoCompra:$("#order-id").html(),DataAceite:o?new Date(o.acceptedAt):null,MaiorIdade:o.accepted})}).then(e=>console.log(e))}else{let s,d;o&&o.accepted||($.cookie("hasAcceptedCookies",JSON.stringify({accepted:!1,createdAt:Date.now(),adulthood:!1}),{path:"/"}),$("body").append(`
+  <div id="cookiebanner">
+      <div id="c-left">
+          <p class="c-header">Protegemos seus dados pessoais</p>
+          <p class="c-message">
+            O Grupo Autoglass, em respeito \xe0 privacidade dos seus dados pessoais e buscando a melhorar sua experi\xeancia de navega\xe7\xe3o no site,
+            gostaria de obter o seu consentimento para coletar e utilizar cookies de navega\xe7\xe3o, que servir\xe3o para trazer conte\xfados personalizados
+            e mais relevantes para voc\xea. Ao clicar em "Aceitar", voc\xea estar\xe1 concordando com a nossa <a href="/Institucional/privacidade" target="_blank">Pol\xedtica de Privacidade.</a>
+          </p>
+      </div>
+      <div id="c-right">
+        <div class="aceitar-cookies-container">
+          <input type="checkbox" id="aceitar-cookies-checkbox" />
+          <label for="aceitar-cookies-checkbox" id="aceitar-cookies-label">Aceitar Cookies</label>
+        </div>
+          <a id="aceitar-cookie-link" class="c-button c-button-disabled">Concordar</a>
+      </div>
+      <div style="clear:both"></div>
+  </div>
+`),s=$("#aceitar-cookies-checkbox"),d=$("#aceitar-cookie-link"),s.change(function(){let e=this.checked;d.toggleClass("c-button-disabled",!e),d.css("background-color",e?"#000000":"#999"),d.css("cursor",e?"pointer":"not-allowed")}),d.click(c),function e(){let o=document.getElementById("cookiebanner"),c=parseInt(o.offsetHeight);o.style.bottom=a-c+"px",(a+=4)<c?setTimeout(function(){e()},1):(a=0,o.style.bottom="10px")}())}},(i=document.createElement("script")).src=e,i.onload=o,document.head.appendChild(i)}();
--- src/js/hubspot-cookie.js
+++ src/js/hubspot-cookie.js
@@ -1,100 +1,104 @@
-!(function () {
+!(function() {
   async function e() {
-    let e = location.href;
-    if (e.includes("checkout/orderPlaced")) {
-      let a = $("#order-id").text().trim(),
-        r = await fetch(`/api/checkout/pub/orders/${a}`, {
-          headers: new Headers({ "Content-Type": "application/json" }),
-        }).then((e) => e.json()),
-        o = r.clientProfileData.email;
-      o && t(o, null, a);
-    }
+      let e = location.href;
+      if (e.includes("checkout/orderPlaced")) {
+          let a = $("#order-id").text().trim(),
+              r = await fetch(`/api/checkout/pub/orders/${a}`, {
+                  headers: new Headers({
+                      "Content-Type": "application/json"
+                  }),
+              }).then((e) => e.json()),
+              o = r.clientProfileData.email;
+          o && t(o, null, a);
+      }
   }
   async function t(e, t = null, a = null) {
-    try {
-      let r = (function e() {
-          try {
-            return $.cookie("hubspotutk");
-          } catch (t) {
-            return null;
-          }
-        })(),
-        o = e.trim(),
-        n =
-          t ||
-          (function e() {
-            try {
-              let t = vtexjs.checkout.orderForm;
-              return t.orderFormId;
-            } catch (a) {
-              return null;
-            }
-          })(),
-        i = `https://api.autoglass.com.br/integracao-b2c/api/web-app/master-datas/clientes/${o}`,
-        l = await fetch(i, {
-          method: "PUT",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify({
-            Hubspotutk: r,
-            LastOrderFormId: n,
-            LastOrderId: a,
-          }),
-        });
-      l.ok
-        ? console.log("Dados enviados com sucesso ao MasterData.")
-        : console.error(
-            "Erro ao enviar dados ao MasterData:",
-            l.status,
-            l.statusText
-          );
-    } catch (c) {
-      console.error("Erro ao enviar dados ao MasterData:", c);
-    }
+      try {
+          let r = (function e() {
+                  try {
+                      return $.cookie("hubspotutk");
+                  } catch (t) {
+                      return null;
+                  }
+              })(),
+              o = e.trim(),
+              n =
+              t ||
+              (function e() {
+                  try {
+                      let t = vtexjs.checkout.orderForm;
+                      return t.orderFormId;
+                  } catch (a) {
+                      return null;
+                  }
+              })(),
+              i = `https://api.autoglass.com.br/integracao-b2c/api/web-app/master-datas/clientes/${o}`,
+              l = await fetch(i, {
+                  method: "PUT",
+                  headers: {
+                      "Content-Type": "application/json"
+                  },
+                  body: JSON.stringify({
+                      Hubspotutk: r,
+                      LastOrderFormId: n,
+                      LastOrderId: a,
+                  }),
+              });
+          l.ok ?
+              console.log("Dados enviados com sucesso ao MasterData.") :
+              console.error(
+                  "Erro ao enviar dados ao MasterData:",
+                  l.status,
+                  l.statusText
+              );
+      } catch (c) {
+          console.error("Erro ao enviar dados ao MasterData:", c);
+      }
   }
   $(window).on("rendered.vtexid", function e(a) {
-    let r = document.querySelector("#vtexIdUI-form-classic-login #inputEmail"),
-      o = document.getElementById("vtexIdUI-form-classic-login");
-    o.addEventListener("submit", (e) => {
-      let a = r.value;
-      a && t(a);
-    });
-  }),
-    $(window).on("checkoutRequestEnd.vtex", function e(a, r) {
-      try {
-        let o = vtexjs.checkout.orderForm,
-          n = "",
-          i = null;
-        if (
-          (r && r.clientProfileData
-            ? ((n = r.clientProfileData.email), (i = r.orderFormId))
-            : o.clientProfileData &&
-              o.clientProfileData.email &&
-              ((n = o.clientProfileData.email), (i = o.orderFormId)),
-          !n)
-        ) {
-          (function e() {
-            let a = location.href;
-            if (a.includes("checkout/#/email")) {
-              let r = document.querySelector(
-                  "#client-profile-data form.client-pre-email"
-                ),
-                o = document.querySelector(
-                  "form.client-pre-email input#client-pre-email"
-                );
-              r.addEventListener("submit", (e) => {
-                let a = o.value;
-                a && t(a);
-              });
-            }
-          })();
-          return;
-        }
-        t(n, i);
-      } catch (l) {
-        console.warn(
-          "Falha ao obter e-mail para enviar o Hubspotutk ao MasterData!"
-        );
-      }
-    }),
-    $(window).on("load", e);
+          let r = document.querySelector("#vtexIdUI-form-classic-login #inputEmail"),
+              o = document.getElementById("vtexIdUI-form-classic-login");
+          o.addEventListener("submit", (e) => {
+              let a = r.value;
+              a && t(a);
+          });
+      }),
+      $(window).on("checkoutRequestEnd.vtex", function e(a, r) {
+          try {
+              let o = vtexjs.checkout.orderForm,
+                  n = "",
+                  i = null;
+              if (
+                  (r && r.clientProfileData ?
+                      ((n = r.clientProfileData.email), (i = r.orderFormId)) :
+                      o.clientProfileData &&
+                      o.clientProfileData.email &&
+                      ((n = o.clientProfileData.email), (i = o.orderFormId)),
+                      !n)
+              ) {
+                  (function e() {
+                      let a = location.href;
+                      if (a.includes("checkout/#/email")) {
+                          let r = document.querySelector(
+                                  "#client-profile-data form.client-pre-email"
+                              ),
+                              o = document.querySelector(
+                                  "form.client-pre-email input#client-pre-email"
+                              );
+                          r.addEventListener("submit", (e) => {
+                              let a = o.value;
+                              a && t(a);
+                          });
+                      }
+                  })();
+                  return;
+              }
+              t(n, i);
+          } catch (l) {
+              console.warn(
+                  "Falha ao obter e-mail para enviar o Hubspotutk ao MasterData!"
+              );
+          }
+      }),
+      $(window).on("load", e);
 })();
