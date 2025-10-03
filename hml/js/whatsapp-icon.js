60f55ffece586c9ceabebc88e742937247f451a6
Elioenai Goncalves De Andrade Nascimento <elioenai.nascimento@autoglass.com.br>
2025-10-03 11:05:35 -0300

    B2C-3867 - Refatoração de todos os links e scripts de todos os templates e subtemplates em HML

--- hml/js/whatsapp-icon.js
+++ hml/js/whatsapp-icon.js
@@ -1,57 +1,32 @@
 (() => {
+  let whatsappIconDesktop = document.querySelector('#whatsapp-icon-link-desktop');
+  let whatsappIconFlutuante = document.querySelector('#whatsapp-icon-link');
 
-    let whatsappIconDesktop = document.querySelector('#whatsapp-icon-link-desktop');
-    let whatsappIconMobile = document.querySelector('#whatsapp-icon-link-mobile');
-    let whatsappIconRodape = document.querySelector('#whatsapp-icon-rodape');
+  const textoConteudoComNumero = "Whatsapp: " + numeroWhatsAppFormatadoAG;
 
-    const textoConteudoComNumero = "Whatsapp: " + numeroWhatsAppFormatadoAG;
+  $('.link-whatsapp').attr('href', urlWhatsAppApi + numeroWhatsAppAG);
+  $('.link-whatsapp-texto').attr('href', urlWhatsAppApi + numeroWhatsAppAG + textoUrlGet);
+  $('.link-whatsapp-conteudo').text(textoConteudoComNumero);
 
-    $('.link-whatsapp').attr('href', urlWhatsAppApi + numeroWhatsAppAG);
-    $('.link-whatsapp-texto').attr('href', urlWhatsAppApi + numeroWhatsAppAG + textoUrlGet);
-    $('.link-whatsapp-conteudo').text(textoConteudoComNumero);
+  if(document.querySelector('.product-qd-v1-fixed-bar')&& whatsappIconFlutuante) {
+      whatsappIconFlutuante.style.marginBottom = '63px'
+  }
 
-    //---------------------------------------------
-    let whatsappIconFlutuante = document.querySelector('#whatsapp-icon-link');
+  let touchEvent = 'ontouchstart' in window ? 'touchstart' : 'click';
 
-    if(document.querySelector('.product-qd-v1-fixed-bar')) {
-        whatsappIconFlutuante.style.marginBottom = '63px'
-    }
-    //---------------------------------------------
+  if (whatsappIconDesktop) {
+    createGAWhatsapp(whatsappIconDesktop, 'Topo Desktop');
+  }
 
-    let touchEvent = 'ontouchstart' in window ? 'touchstart' : 'click';
-
-    createGAWhatsapp(whatsappIconDesktop, 'Topo');
-    createGAWhatsapp(whatsappIconMobile, 'Topo');
+  if (whatsappIconFlutuante) {
     createGAWhatsapp(whatsappIconFlutuante, 'Flutuante');
-    createGAWhatsapp(whatsappIconRodape, 'Rodapé');
+  }
 
-    function createGAWhatsapp (icon, device) {
-        icon.addEventListener(touchEvent, (_) => {
-            ga('create', 'UA-133498560-1', 'autoglassonline.com', 'gaTracker');
-            ga('gaTracker.set', 'transport', 'beacon');
-            ga('gaTracker.send', 'event', 'WhatsApp', 'Clique', `${device}`);
-        }, {passive: true});
-    }
-
-    // const addBorderZDButton = () => { //ZENDESK -- Remover caso deixe de usar
-    //     let iframe = document.querySelector('#launcher');
-    //     zdFrame = document.querySelector('#launcher').contentDocument
-
-    //     if(!zdFrame) {
-    //         return;
-    //     }
-
-    //     iframe.style.margin = '5px 20px';
-    //     zdFrame.querySelector('.u-userLauncherColor').style = 'border: 4px solid #FFF !important';
-    // }
-
-    // setTimeout(addBorderZDButton, 5000);
-
-    function dispositivoMovel() {
-      const windowWidth = window.innerWidth
-        || document.documentElement.clientWidth
-        || document.body.clientWidth;
-      return windowWidth < 1200;
-    }
-
+  function createGAWhatsapp (icon, device) {
+      icon.addEventListener(touchEvent, (_) => {
+          ga('create', 'UA-133498560-1', 'autoglassonline.com', 'gaTracker');
+          ga('gaTracker.set', 'transport', 'beacon');
+          ga('gaTracker.send', 'event', 'WhatsApp', 'Clique', `${device}`);
+      }, {passive: true});
+  }
 })();
