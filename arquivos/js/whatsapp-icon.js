299bff97ceabdb8ab9f0241deea62d8ea9b1c4f0
Elioenai Goncalves De Andrade Nascimento <elioenai.nascimento@autoglass.com.br>
2025-10-13 23:15:55 -0300

    B2C-3870 - Ajusta erros de console com whatsapp-icon.js em PROD

--- arquivos/js/whatsapp-icon.js
+++ arquivos/js/whatsapp-icon.js
@@ -1,8 +1,6 @@
 (() => {
-
   let whatsappIconDesktop = document.querySelector('#whatsapp-icon-link-desktop');
-  let whatsappIconMobile = document.querySelector('#whatsapp-icon-link-mobile');
-  let whatsappIconRodape = document.querySelector('#whatsapp-icon-rodape');
+  let whatsappIconFlutuante = document.querySelector('#whatsapp-icon-link');
 
   const textoConteudoComNumero = "Whatsapp: " + numeroWhatsAppFormatadoAG;
 
@@ -10,20 +8,19 @@
   $('.link-whatsapp-texto').attr('href', urlWhatsAppApi + numeroWhatsAppAG + textoUrlGet);
   $('.link-whatsapp-conteudo').text(textoConteudoComNumero);
 
-  //---------------------------------------------
-  let whatsappIconFlutuante = document.querySelector('#whatsapp-icon-link');
-
-  if(document.querySelector('.product-qd-v1-fixed-bar')) {
+  if(document.querySelector('.product-qd-v1-fixed-bar')&& whatsappIconFlutuante) {
       whatsappIconFlutuante.style.marginBottom = '63px'
   }
-  //---------------------------------------------
 
   let touchEvent = 'ontouchstart' in window ? 'touchstart' : 'click';
 
-  createGAWhatsapp(whatsappIconDesktop, 'Topo');
-  createGAWhatsapp(whatsappIconMobile, 'Topo');
-  createGAWhatsapp(whatsappIconFlutuante, 'Flutuante');
-  createGAWhatsapp(whatsappIconRodape, 'Rodapé');
+  if (whatsappIconDesktop) {
+    createGAWhatsapp(whatsappIconDesktop, 'Topo Desktop');
+  }
+
+  if (whatsappIconFlutuante) {
+    createGAWhatsapp(whatsappIconFlutuante, 'Flutuante');
+  }
 
   function createGAWhatsapp (icon, device) {
       icon.addEventListener(touchEvent, (_) => {
@@ -32,26 +29,4 @@
           ga('gaTracker.send', 'event', 'WhatsApp', 'Clique', `${device}`);
       }, {passive: true});
   }
-
-  // const addBorderZDButton = () => { //ZENDESK -- Remover caso deixe de usar
-  //     let iframe = document.querySelector('#launcher');
-  //     zdFrame = document.querySelector('#launcher').contentDocument
-
-  //     if(!zdFrame) {
-  //         return;
-  //     }
-
-  //     iframe.style.margin = '5px 20px';
-  //     zdFrame.querySelector('.u-userLauncherColor').style = 'border: 4px solid #FFF !important';
-  // }
-
-  // setTimeout(addBorderZDButton, 5000);
-
-  function dispositivoMovel() {
-    const windowWidth = window.innerWidth
-      || document.documentElement.clientWidth
-      || document.body.clientWidth;
-    return windowWidth < 1200;
-  }
-
 })();
